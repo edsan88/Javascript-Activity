771d37da8b6d3cad5b71bd904410a5636f3527e2
3rd Year Account_08 <3rdyearaccount@KINITKYUTEST.local>
2016-07-23 12:09:53 +0800

    first comment

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var title_heading = document.getElementById('myTitleHeading');
+title_heading.innerHTML = "My Javascript New Title";
+
+var title_sub = document.getElementById('mySubtitle');
+title_sub.innerHTML = "My New Sub-Title";
+
+var table_new = document.getElementById('table_new');
+table_new.innerHTML = "<tr><td>1</td><td>Test</td></tr><tr><td>2</td><td>Test2</td></tr>";
